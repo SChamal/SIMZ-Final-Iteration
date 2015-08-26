664553c705dcd857d85c3a014f2edb76a5f73f8d
NuwanDahanayake <nuwandahanayake03@gmail.com>
2015-08-26 16:33:46 +0530

    init

---
+++ src/simz2/Simz.java
@@ -0,0 +1,13 @@
+package simz2;
+
+
+public class Simz {
+    
+    public static void main(String[] args) {
+        LoginFrame1 lf = new LoginFrame1();
+        lf.setSize(755, 610);
+        lf.setVisible(true);
+        lf.btnHint.setVisible(false);
+    }
+    
+}
